afdd35c1382058882b919002f78111eefccaf9f2
Jesse Alvarez <jesse@Jesses-MBP.home>
2022-02-22 11:09:56 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync');
+
+    while (let i = 0; i <=10; i++){
+        let itemx = prompt('Add item to your to-do list:')
+
+    }
+// Start off your project by creating a loop that loops forever (use a while loop). Inside this loop you'll want to do three things:
+// 1. Display content to the user
+// 2. Prompt the user for an action
+// 3. Handle that action accordingly
